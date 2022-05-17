3044d3541de3e9c148c2ab22dcfd0f352c7fd485
Baschounette <78910786+Baschounette@users.noreply.github.com>
2022-05-17 16:05:02 +0200

    ajout de courses et modif

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,35 @@ import { Injectable } from '@angular/core';
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  getCoursesFromService(){
+    return this.http.get('http://localhost:8080/Course/allValid');
+  }
+
+  addCoursesFromService(course: any) {
+    return this.http.post("http://localhost:8080/Course/add", course);
+  }
+
+  addReservationFromService(reservation: any){
+    return this.http.post('http://localhost:8080/Reservation/add', reservation);
+  }
+  searchByCityService(city: any) {
+    let keyCity = city.value.keyCity;
+    return this.http.get('http://localhost:8080/Course/searchcity/' + keyCity)
+  }
+
+  getMyCoursesFromService() {
+    return this.http.get('http://localhost:8080/Reservation/all');
+  }
+
+  getPageOfMyCoursesFromService(page: any) {
+    return this.http.get('http://localhost:8080/Reservation/Page?page='+page)
+  }
+
+  getPageMax() {
+    return this.http.get('http://localhost:8080/Reservation/pagemax')
+  }
+
 }
